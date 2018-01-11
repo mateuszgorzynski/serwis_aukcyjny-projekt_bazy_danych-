4eb18be1fc4e25281747d130f13cb3abefc022c7
adr7dev <adr7dev@gmail.com>
2018-01-11 14:20:57 +0100

    update ProcedureAddOpinions and create ProcedureDeleteOpinions

--- ScriptProcedureAddOpinions.sql
+++ ScriptProcedureAddOpinions.sql
@@ -19,6 +19,13 @@ AS
 	IF NOT EXISTS (SELECT * FROM adr7dev_mateusz.Transactions WHERE IdTransaction = @IdTransaction)
 		RAISERROR (N'Transaction with given IdTransaction does not exist.', 16,  1)
 
+	IF NOT EXISTS 
+	(SELECT * FROM adr7dev_mateusz.Transactions t 
+	INNER JOIN adr7dev_mateusz.Items i 
+	ON i.IdItem = t.IdItem 
+	WHERE IdTransaction = @IdTransaction AND Seller = @IdSeller)
+		RAISERROR (N'IdSeller must be authentical seller of transaction which id is IdTransaction.', 16, 1)
+
 	INSERT INTO adr7dev_mateusz.Opinions(IdSeller, IdTransaction, Note, Description)
 	VALUES (@IdSeller, @IdTransaction, @Note, @Decsription)
 
@@ -26,7 +33,7 @@ GO
 
 BEGIN TRY
 	-- PATTERN (REPLACE # WITH CORRECT VALUE)
-	-- EXEC InsertOpinion @IdSeller = #, @IdTransaction = #, @Note = #, @Decsription = #
+	-- EXEC InsertOpinion @IdSeller = 1, @IdTransaction = 1, @Note = 0, @Decsription = 'chujowka'
 END TRY
 BEGIN CATCH
         SELECT  ERROR_NUMBER() AS ErrorNumber ,ERROR_MESSAGE() AS ErrorMessage;
---
+++ ScriptProcedureDeleteOpinions.sql
@@ -0,0 +1,23 @@
+﻿IF OBJECT_ID('DeleteOpinion','P') IS NOT NULL
+	DROP PROCEDURE DeleteOpinion
+GO
+
+CREATE PROCEDURE DeleteOpinion (
+	@IdOpinion INT
+	)
+AS
+
+	IF NOT EXISTS (SELECT * FROM adr7dev_mateusz.Opinions WHERE IdOpinion = @IdOpinion)
+		RAISERROR (N'Opinion with given IdOpinion does not exist.', 16, 1)
+
+	DELETE FROM adr7dev_mateusz.Opinions WHERE IdOpinion = @IdOpinion
+
+GO
+
+BEGIN TRY
+	-- PATTERN (REPLACE # WITH CORRECT VALUE)
+	-- EXEC DeleteOpinion @IdOpinion = #
+END TRY
+BEGIN CATCH
+        SELECT  ERROR_NUMBER() AS ErrorNumber ,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
