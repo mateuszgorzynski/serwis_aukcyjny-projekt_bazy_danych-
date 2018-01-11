b934629261e771263ffe9685ec56e4febb91d4fd
adr7dev <adr7dev@gmail.com>
2018-01-11 15:33:00 +0100

    comment in AveragreProfit function

--- ScriptFunctionAverageProfit.sql
+++ ScriptFunctionAverageProfit.sql
@@ -6,6 +6,7 @@ begin
 	RETURN (SELECT AVG(t.Price) FROM adr7dev_mateusz.Transactions t INNER JOIN adr7dev_mateusz.Items i ON t.IdItem = i.IdItem WHERE Seller = @IdUser)
 end
 
+-- PATTERN
 -- SELECT *, dbo.FunctionAverageProfit(IdUser) as AverageProfit FROM Users
 
 -- DROP FUNCTION FunctionAverageProfit
