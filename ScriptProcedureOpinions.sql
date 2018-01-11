ad91c1f886ebaf5e751402776901f8f6c26e7dd2
adr7dev <adr7dev@gmail.com>
2018-01-11 12:20:26 +0100

    Removed unnecessary comments and blocks in ProcedureOpinions

--- ScriptProcedureOpinions.sql
+++ ScriptProcedureOpinions.sql
@@ -1,43 +1,32 @@
-IF OBJECT_ID('InsertOpinion','P') IS NOT NULL
-DROP PROCEDURE InsertOpinion
+IF OBJECT_ID('InsertOpinion', 'P') IS NOT NULL
+	DROP PROCEDURE InsertOpinion
 GO
 
 CREATE PROCEDURE InsertOpinion (
-        @IdSeller INT ,
-        @IdTransaction INT ,
-        @Note BINARY ,
-        @Decsription TEXT)
+	@IdSeller INT,
+	@IdTransaction INT,
+	@Note BINARY,
+	@Decsription TEXT
+	)
 AS
 
---1. If someone is empty or wrong written -> call the error and finish the procedur
-        /*IF (@Decsription IS NULL)     
-		BEGIN
-                RAISERROR (N'Decsription is empty, or wrong written. %s', 16,  1, -- State
-                        @Decsription)
-		END*/
-		 IF (@Note IS NULL)     
-		BEGIN
-                RAISERROR (N'Note is empty. %s', 16,  1,  @Note)
-        END
+	IF (@Note IS NULL)     
+		RAISERROR (N'Note is empty.', 16,  1)
 		
-		IF NOT EXISTS(SELECT * FROM adr7dev_mateusz.Users WHERE IdUser = @IdSeller  )
-		BEGIN
-				RAISERROR (N'This Seller not exists. %s', 16,  1,  @IdSeller)
-		END
+	IF NOT EXISTS (SELECT * FROM adr7dev_mateusz.Users WHERE IdUser = @IdSeller)
+		RAISERROR (N'This Seller not exists.', 16, 1)
 
-		IF NOT EXISTS(SELECT * FROM adr7dev_mateusz.Transactions WHERE IdTransaction = @IdTransaction  )
-		BEGIN
-				RAISERROR (N'This Transaction not exists. %s', 16,  1,  @IdTransaction)
-		END
+	IF NOT EXISTS (SELECT * FROM adr7dev_mateusz.Transactions WHERE IdTransaction = @IdTransaction)
+		RAISERROR (N'This Transaction not exists.', 16,  1)
 
-		--7. Insert new opinion:
-      INSERT INTO adr7dev_mateusz.Opinions( IdSeller,IdTransaction,Note,Description)
-        VALUES (@IdSeller, @IdTransaction, @Note, @Decsription)
+	INSERT INTO adr7dev_mateusz.Opinions(IdSeller, IdTransaction, Note, Description)
+	VALUES (@IdSeller, @IdTransaction, @Note, @Decsription)
 
 GO
 
 BEGIN TRY
-        EXEC InsertOpinion @IdSeller = 1, @IdTransaction=1, @Note=1, @Decsription='Pikny'
+	-- PATTERN (REPLACE # WITH CORRECT VALUE)
+	-- EXEC InsertOpinion @IdSeller = #, @IdTransaction = #, @Note = #, @Decsription = #
 END TRY
 BEGIN CATCH
         SELECT  ERROR_NUMBER() AS ErrorNumber ,ERROR_MESSAGE() AS ErrorMessage;
