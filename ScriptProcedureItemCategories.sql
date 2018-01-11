ee00b37caee4ec6540bbac0629126cff84de701c
adr7dev <adr7dev@gmail.com>
2018-01-11 12:12:09 +0100

    Removed unnecessary comments and blocks in ProcedureItemCategories

--- ScriptProcedureItemCategories.sql
+++ ScriptProcedureItemCategories.sql
@@ -3,68 +3,39 @@ DROP PROCEDURE InsertItemCategory
 GO
 
 CREATE PROCEDURE InsertItemCategory (
-        @Name VARCHAR(250) ,
+        @Name VARCHAR(250),
         @ParentCategory INT
-        )
+		)
 AS
-		--declare @IdCategory
 		DECLARE @IdCategory INT
 		 
-                 -- verification of internal parameters --
-        --1. If Name is empty or wrong written -> call the error and finish the procedur
         IF (@Name IS NULL)     
-		BEGIN
-                RAISERROR (N'First Name is empty. %s', -- Message text.
-                        16, -- Severity, severity 11-19 will cause execution to jump to CATCH block
-                        1, -- State
-                        @Name 
-						)
-        END
+			RAISERROR (N'First Name is empty.', 16, 1)
 
-		
-
-		
-
-		 --2. Set New I wowowowowowowow
         SET @IdCategory = (SELECT ISNULL(MAX(IdCategory),0)+1 FROM adr7dev_mateusz.ItemCategories)
 
 		IF @IdCategory = @ParentCategory
-		BEGIN
-                RAISERROR (N'IdCategory and ParentCategory cant be same value . %s', -- Message text.
-                        16, -- Severity, severity 11-19 will cause execution to jump to CATCH block
-                        1, -- State
-                        @ParentCategory
-                )
-        END
+			RAISERROR (N'IdCategory and ParentCategory cant be same value . %s', 16, 1, @ParentCategory)
 
-		if (@ParentCategory =0)
+		IF (@ParentCategory = 0)
 		BEGIN
-		INSERT INTO adr7dev_mateusz.ItemCategories([Name])
-        VALUES (@Name)
+			INSERT INTO adr7dev_mateusz.ItemCategories([Name])
+			VALUES (@Name)
 		END
+
 		ELSE
 
-			BEGIN
-			
+		BEGIN
 			INSERT INTO adr7dev_mateusz.ItemCategories([Name],ParentCategory)
 			VALUES (@Name,@ParentCategory)
-
-			END
-	
-
+		END
 GO
 
 
 BEGIN TRY
-        
-		EXEC InsertItemCategory @Name = 'Elektronika', @ParentCategory =0
-		EXEC InsertItemCategory @Name = 'laptopy', @ParentCategory =1
-		EXEC InsertItemCategory @Name = 'teleony', @ParentCategory =1
-
-
+        -- PATTERN (REPLACE # WITH CORRECT VALUE)
+		--EXEC InsertItemCategory @Name = #, @ParentCategory = #
 END TRY
 BEGIN CATCH
         SELECT  ERROR_NUMBER() AS ErrorNumber ,ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
-
-select * from adr7dev_mateusz.ItemCategories
