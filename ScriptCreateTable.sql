3fc40de3d711aa3bae68bcf7bb0d07abd28c0164
adr7dev <adr7dev@gmail.com>
2018-01-11 01:02:17 +0100

    script which creates table
    Table Items and Auctions

--- ScriptCreateTable.sql
+++ ScriptCreateTable.sql
@@ -1,5 +1,7 @@
-DROP table adr7dev_mateusz.Users
-DROP table adr7dev_mateusz.ItemCategories
+DROP TABLE adr7dev_mateusz.Auctions
+DROP TABLE adr7dev_mateusz.Items
+DROP TABLE adr7dev_mateusz.Users
+DROP TABLE adr7dev_mateusz.ItemCategories
 
 CREATE TABLE Users(
 IdUser INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
@@ -12,8 +14,30 @@ City VARCHAR CHECK (City LIKE '[A-Z]%'),
 PostCode VARCHAR(6) CHECK (PostCode LIKE '[0-9][0-9]-[0-9][0-9][0-9]%') NOT NULL
 )
 CREATE TABLE ItemCategories(
-IdCategories INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+IdCategory INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 CategoryName VARCHAR CHECK (CategoryName LIKE '[A-Z]%') NOT NULL,
-ParentCategory INT not null,
-CONSTRAINT FKCategoryItem FOREIGN KEY (ParentCategory) REFERENCES ItemCategories(IdCategories),
+ParentCategory INT NOT NULL,
+CONSTRAINT FKParentCategory FOREIGN KEY (ParentCategory) REFERENCES ItemCategories(IdCategory),
+)
+CREATE TABLE Items(
+IdItem INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+ItemName VARCHAR NOT NULL,
+ItemDescription TEXT,
+Category INT NOT NULL,
+Seller INT NOT NULL,
+TheType BIT NOT NULL,
+Price INT NOT NULL,
+MinRaise INT NOT NULL,
+FinishDate DATETIME NOT NULL,
+CONSTRAINT FKCategory FOREIGN KEY (Category) REFERENCES ItemCategories(IdCategory),
+CONSTRAINT FKSeller FOREIGN KEY (Seller) REFERENCES Users(IdUser),
+)
+CREATE TABLE Auctions(
+IdAuction INT PRIMARY KEY IDENTITY(1,1),
+IdItem INT NOT NULL,
+IdBuyer INT NOT NULL,
+Price INT NOT NULL,
+BiddingDate DATETIME NOT NULL,
+CONSTRAINT FKItem FOREIGN KEY (IdItem) REFERENCES Items(IdItem),
+CONSTRAINT FKBuyer FOREIGN KEY (IdBuyer) REFERENCES Users(IdUser),
 )
