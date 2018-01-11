08885fee5f78a70b28ad771ef2a50f17fa1ff137
adr7dev <adr7dev@gmail.com>
2018-01-11 15:30:17 +0100

    create functions (AverageProfit and GetItemsByCategory)

---
+++ ScriptFunctionAverageProfit.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION dbo.FunctionAverageProfit
+	(@IdUser INT)
+	returns INT
+as
+begin    
+	RETURN (SELECT AVG(t.Price) FROM adr7dev_mateusz.Transactions t INNER JOIN adr7dev_mateusz.Items i ON t.IdItem = i.IdItem WHERE Seller = @IdUser)
+end
+
+-- SELECT *, dbo.FunctionAverageProfit(IdUser) as AverageProfit FROM Users
+
+-- DROP FUNCTION FunctionAverageProfit
---
+++ ScriptFunctionGetItemsByCategory.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION dbo.FunctionGetItemsByCategory
+	(@IdCategory INT)
+	RETURNS TABLE
+AS
+	RETURN SELECT * FROM adr7dev_mateusz.Items WHERE Category = @IdCategory AND IsBlocked = 0 AND FinishDate > GETDATE()
+
+
+-- PATTERN
+-- SELECT * FROM FunctionGetItemsByCategory(1)
+
+-- DROP FUNCTION dbo.FunctionGetItemsByCategory
